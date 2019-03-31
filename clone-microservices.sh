1393f9be4713c7536c8ec71ebcbbccbff4fc37eb
Eduard Predescu <eduard.florin.predescu@gmail.com>
2019-03-31 13:47:25 +0300

    added clone all repos command

---
+++ clone-microservices.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+git clone git@gitlab.com:back-devs/schoolar/schoolar-live_qa-service.git
+git clone git@gitlab.com:back-devs/schoolar/schoolar-quiz-service.git
+git clone git@gitlab.com:back-devs/schoolar/schoolar-user-service.git
+git clone git@gitlab.com:back-devs/schoolar/schoolar-schedule-service.git
